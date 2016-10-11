a5f014cbab3ee7613cdf7ad0c507d052475ba33f
Daniel Rejon <dandarkus7@gmail.com>
2016-10-11 13:25:47 -0500

    New class Queue

---
+++ Queue.php
@@ -0,0 +1,29 @@
+<?php 
+require_once 'LinkedList.php';
+
+class Queue extends LinkedList {
+
+	/**
+	 * head:
+	 * Return the first element of the queue
+	 */
+	function head() {
+
+		if ($this->firstNode == null) {
+			return null;
+		} else {
+			return $this->firstNode->getData();
+		}
+
+	}
+
+	/**
+	 * Override ->
+	 * shift:
+	 * add:
+	 */
+
+}
+
+
+ ?>
